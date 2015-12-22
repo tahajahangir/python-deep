b349e9cbad7b8240c906de48dc0481be74ce27b2
Taha Jahangir <mtjahangir@gmail.com>
2015-12-22 20:05:47 +0330

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,11 @@ setup(name='deep',
       description="Easy, flexible deep comparison and testing of structured data",
       url="http://code.google.com/p/python-deep/",
       download_url="http://code.google.com/p/python-deep/downloads/list",
+      classifiers="""
+          Topic :: Software Development :: Testing
+          Programming Language :: Python :: 2
+          Programming Language :: Python :: 3
+          License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
+      """.strip().split('\n'),
       license="LGPL",
       )
